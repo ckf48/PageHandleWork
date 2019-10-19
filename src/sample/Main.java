7f22d902818fb17d0b611366118223dac9a3ad67
ckf48 <3048764886@qq.com>
2019-10-19 15:30:58 +0800

    Interface complete

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("PageHandle");
-        primaryStage.setScene(new Scene(root, 630, 650));
+        primaryStage.setScene(new Scene(root, 630, 550));
         primaryStage.show();
     }
 
